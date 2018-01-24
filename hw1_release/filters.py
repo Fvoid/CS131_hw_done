b3d1a7f3f4222526611a1cff67530462d7c56455
Unknown <ganbate.wang@gmail.com>
2018-01-23 23:48:24 -0500

    done hw1

--- hw1_release/filters.py
+++ hw1_release/filters.py
@@ -19,8 +19,32 @@ def conv_nested(image, kernel):
     Hk, Wk = kernel.shape
     out = np.zeros((Hi, Wi))
 
+
+    #-1-kernel at the left top corner -2- right top corner -3- left bottom
+    #-4- rihgt bottom -5- left side -6- right side -7-normal
+
+    imagec = image.copy()
+    imagec = zero_pad(imagec, 1, 1)
+    hRange = int((Hk - 1) / 2)
+    wRange = int((Wk - 1) / 2)
     ### YOUR CODE HERE
-    pass
+    for imageH in range(Hi):
+        for imageW in range(Wi):
+            sum1 = 0.0
+            kerH = 0
+            kerW = 0
+            for covH in range(imageH-hRange, imageH+hRange+1):
+                for covW in range(imageW-hRange, imageW+wRange+1):
+                    if covH < Hi and covW < Wi and covH >= 0 and covW >= 0:
+                        sum1 += image[covH, covW] * kernel[kerH, kerW]
+                    kerW += 1
+                    if kerW == Wk:
+                        kerW = 0
+                        kerH += 1
+                    if kerH == Hk:
+                        kerH =0
+            out[imageH, imageW] = sum1
+
     ### END YOUR CODE
 
     return out
@@ -47,7 +71,7 @@ def zero_pad(image, pad_height, pad_width):
     out = None
 
     ### YOUR CODE HERE
-    pass
+    out = np.pad(image, [(pad_height,), (pad_width, )], mode='constant')
     ### END YOUR CODE
     return out
 
@@ -74,9 +98,30 @@ def conv_fast(image, kernel):
     Hi, Wi = image.shape
     Hk, Wk = kernel.shape
     out = np.zeros((Hi, Wi))
+    padH = 0
+    padW = 0
 
-    ### YOUR CODE HERE
-    pass
+    imagec = image.copy()
+
+    if Hk % 2 != 0:
+        padH = int((Hk - 1) / 2)
+    else:
+        padH = int(Hk / 2)
+
+    if Wk % 2 != 0:
+        padW = int((Wk - 1) / 2)
+    else:
+        padW = int(Wk / 2)
+
+    imagec = zero_pad(imagec, padH, padW)
+
+    for imageH in range(Hi):
+        for imageW in range(Wi):
+            imagecH = imageH + padH
+            imagecW = imageW + padW
+
+            imageSub = imagec[(imagecH-padH):(imagecH+padH+1), (imagecW-padW):(imagecW+padW+1)]
+            out[imageH, imageW] = np.sum(imageSub * kernel)
     ### END YOUR CODE
 
     return out
@@ -115,10 +160,11 @@ def cross_correlation(f, g):
 
     out = None
     ### YOUR CODE HERE
-    pass
-    ### END YOUR CODE
+    out = conv_fast(f, np.flip(g[1:,], 1))
+
 
     return out
+
 
 def zero_mean_cross_correlation(f, g):
     """ Zero-mean cross-correlation of f and g
@@ -135,7 +181,9 @@ def zero_mean_cross_correlation(f, g):
 
     out = None
     ### YOUR CODE HERE
-    pass
+    meanVal = g[1:].mean()
+    updateG = g[1:] - meanVal
+    out = conv_fast(f, updateG)
     ### END YOUR CODE
 
     return out
@@ -156,7 +204,40 @@ def normalized_cross_correlation(f, g):
 
     out = None
     ### YOUR CODE HERE
-    pass
+    image = f
+    kernel = g
+    Hi, Wi = image.shape
+    Hk, Wk = kernel.shape
+    out = np.zeros((Hi, Wi))
+    padH = 0
+    padW = 0
+
+    imagec = image.copy()
+
+    if Hk % 2 != 0:
+        padH = int((Hk - 1) / 2)
+    else:
+        padH = int(Hk / 2)
+
+    if Wk % 2 != 0:
+        padW = int((Wk - 1) / 2)
+    else:
+        padW = int(Wk / 2)
+
+    imagec = zero_pad(imagec, padH, padW)
+    kerMean = kernel.mean()
+    kerStd = np.std(kerMean)
+
+    for imageH in range(Hi):
+        for imageW in range(Wi):
+            imagecH = imageH + padH
+            imagecW = imageW + padW
+
+            imageSub = imagec[(imagecH-padH):(imagecH+padH+1), (imagecW-padW):(imagecW+padW+1)]
+            imageMean = imageSub.mean()
+            imageStd = np.std(imageSub)
+
+            out[imageH, imageW] = np.sum((imageSub-imageMean) / imageStd * kernel)
     ### END YOUR CODE
 
     return out
